a875fc2324ff37f5974554e798889941ee64bdf4
zxmpg41 <kellenberg.kevin@gmail.com>
2023-08-07 23:10:17 +0200

    add new limit to transfer graph

--- frontend/src/components/TransferRevenueLineChart.js
+++ frontend/src/components/TransferRevenueLineChart.js
@@ -31,7 +31,7 @@ function TransferRevenueLineChart(props) {
         yScale={{
           type: 'linear',
           stacked: false,
-          min: -100000,
+          min: -1500000,
         }}
         yFormat={(value) =>
           `${new Intl.NumberFormat('de-DE', {
