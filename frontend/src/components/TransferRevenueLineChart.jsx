d14365e54a3888f628527dfc5cf0ed10004ddc15
Kevin Kellenberger <48241288+zxmpg41@users.noreply.github.com>
2023-08-18 11:14:00 +0200

    increase negative limit in revenue graph

--- frontend/src/components/TransferRevenueLineChart.jsx
+++ frontend/src/components/TransferRevenueLineChart.jsx
@@ -31,7 +31,7 @@ function TransferRevenueLineChart(props) {
         yScale={{
           type: 'linear',
           stacked: false,
-          min: -2500000,
+          min: -3500000,
         }}
         yFormat={(value) =>
           `${new Intl.NumberFormat('de-DE', {
